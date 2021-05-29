fb8509284902d705a64006f462ed07a11f3da9ef
ioerrors <55862966+ioerrors@users.noreply.github.com>
2021-05-29 12:54:54 -0700

    customer and hashtable first iterations

--- hashTable.h
+++ hashTable.h
@@ -1,40 +1,27 @@
-////////////////////////////////tree.h file  //////////////////////////////////
+////////////////////////////////hashTable.h file  /////////////////////////////
 //-----------------------------------------------------------------------------
-// Created by Group 1 on 05/19/2021.
+// Created by Micah Rice and Abraham Sham on 05/25/2021.
 //-----------------------------------------------------------------------------
-// This class is a data structure to hold movies in a sorted format
+// This class is a data structure to hold customers
+// in a quickly accessable format
 //-----------------------------------------------------------------------------
 
-struct Node { 				// BST Node contains pointer for left, right, and movie
-    Node* left;
-    Node* right;
-    Movie* movie;
-};
-
-class BST {
+class HashTable {
     
-    
-    friend ostream& operator<< (ostream&, const BST&); 	// Overloaded << : prints BST in correct structure
+    friend ostream& operator<< (ostream&, const HashTable&); 	// Overloaded << : prints HashTable in correct structure
     
 public:
     
-    BST ();             // constructor
-    ~BST ();            // desctructor
+    HashTable ();             // constructor
+    ~HashTable ();            // desctructor
     
-    bool getMovie (const Movie&, Movie*&) const;            // get movie from the tree
-    bool addMovie(Movie*&);                                 // add movie node
-    bool getMovieByTitle (string, int, Movie*&);            // returns true if movie is found in BSTree by its title
 
-    void resetCounted();                                    // reset movie counted status to false
+    //find
+    bool getCustomer (const int cID, Customer*&) const;            // get Customer from the tree
     
+    //insert
+    bool addCustomer(Customer*&);                                 // add Customer node
     
 private:
-    Node* root;                                 // points to the root of the BST
-    
-    void makeEmptyHelper (Node* &);             // private functions
-    void printHelpNode (Node* cur) const;         // helper method for print
-    bool insertHelper (Node *&, Movie*&);       // helper method for insert
-    bool retrieveHelper (const Movie&, Movie*& , Node*) const;   // helper method for retrieve
-    void resetHelper(Node*);                    // helper method for reset
-    bool getMovieByTitleHelper (string title, int year, Movie*&, Node*);  // helper method for getMovieByTitle
+
 };
